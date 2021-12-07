64e06da9fc7a8a8ad66202aef03a6b4e951fd208
Berk Sayin <berkosayin@gmail.com>
2021-12-07 16:44:37 +0300

    Admin, artist and web mvc projects are added.

---
+++ Stopify.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stopify.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
